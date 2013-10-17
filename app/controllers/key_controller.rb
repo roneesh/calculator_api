148dc6616f8d19a3465ac0796e441db003973f12
Roneesh Vashisht <roneesh@gmail.com>
2013-10-17 18:56:25 -0500

    refactoring operators action

--- app/controllers/key_controller.rb
+++ app/controllers/key_controller.rb
@@ -12,49 +12,15 @@ class KeyController < ApplicationController
       render json: "Please pass in a query string parameter"
     end
 
-    # if params[:query] == "1"
-    #   render json: 1
-    # elsif params[:query] == "2"
-    #   render json: 2
-    # elsif params[:query] == "3"
-    #   render json: 3
-    # elsif params[:query] == "4"
-    #   render json: 4
-    # elsif params[:query] == "5"
-    #   render json: 5
-    # elsif params[:query] == "6"
-    #   render json: 6
-    # elsif params[:query] == "7"
-    #   render json: 7
-    # elsif params[:query] == "8"
-    #   render json: 8
-    # elsif params[:query] == "9"
-    #   render json: 9
-    # elsif params[:query] == "" || params[:query] == nil
-    #   render json: "Please pass in a query string parameter"
-    # end
-
   end
 
   def operator
 
-    if params[:query] == "add"
-      render json: "+"
-    elsif params[:query] == "subtract"
-      render json: "-"
-    elsif params[:query] == "multiply"
-      render json: "x"
-    elsif params[:query] == "divide"
-      render json: "/"
-    elsif params[:query] == "C"
-      render json: "C"
-    elsif params[:query] == "AC"
-      render json: "AC"
-    elsif params[:query] == "equals"
-      render json: "="
-    elsif params[:query] == "decimal"
-      render json: "."
-    elsif params[:query] == "" || params[:query] == nil
+    operators = {"add" => "+" , "subtract" => "-", "multiply" => "x", "divide" => "/", "C" => "C", "AC" => "AC", "decimal" => ".", "equals" => "=" }
+
+    if operators.has_key? params[:query]
+      render json: operators[params[:query]]
+    else
       render json: "Please pass in a query string parameter"
     end
 
