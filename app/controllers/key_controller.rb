1633264cbd07e1b00650012161305f7fce75cf6d
Roneesh Vashisht <roneesh@gmail.com>
2013-10-17 18:53:08 -0500

    refactored digit action

--- app/controllers/key_controller.rb
+++ app/controllers/key_controller.rb
@@ -2,27 +2,37 @@ class KeyController < ApplicationController
 
   def digit
   
-    if params[:query] == "1"
-      render json: 1
-    elsif params[:query] == "2"
-      render json: 2
-    elsif params[:query] == "3"
-      render json: 3
-    elsif params[:query] == "4"
-      render json: 4
-    elsif params[:query] == "5"
-      render json: 5
-    elsif params[:query] == "6"
-      render json: 6
-    elsif params[:query] == "7"
-      render json: 7
-    elsif params[:query] == "8"
-      render json: 8
-    elsif params[:query] == "9"
-      render json: 9
-    elsif params[:query] == "" || params[:query] == nil
+    (1..9).each do |number|
+      if params[:query] == number.to_s
+        render json: number
+      end
+    end
+
+    if params[:query] == nil || params[:query] == ""
       render json: "Please pass in a query string parameter"
     end
+
+    # if params[:query] == "1"
+    #   render json: 1
+    # elsif params[:query] == "2"
+    #   render json: 2
+    # elsif params[:query] == "3"
+    #   render json: 3
+    # elsif params[:query] == "4"
+    #   render json: 4
+    # elsif params[:query] == "5"
+    #   render json: 5
+    # elsif params[:query] == "6"
+    #   render json: 6
+    # elsif params[:query] == "7"
+    #   render json: 7
+    # elsif params[:query] == "8"
+    #   render json: 8
+    # elsif params[:query] == "9"
+    #   render json: 9
+    # elsif params[:query] == "" || params[:query] == nil
+    #   render json: "Please pass in a query string parameter"
+    # end
 
   end
 
